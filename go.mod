d2f1e7f73509ad0b7bc1299bb4c4876369f4450b
guacamole <gunjanwalecha@gmail.com>
2021-05-09 18:18:30 +0530

    working now
    Signed-off-by: guacamole <gunjanwalecha@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/guacamole/boyer-moore-string-searching
 
 go 1.16
+
+require github.com/fatih/color v1.10.0 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
 	////////01234567890123456789010123456789012345678901234567890
-	str := `any sentence will do apples are not oranges but bananas they aare mangoes`
-	pattern := "are"
+	byt,err := ioutil.ReadFile("./input.txt")
+	if err!= nil {
+		fmt.Println("error reading from file",err)
+		os.Exit(1)
+	}
+
+	str := string(byt)
+	strcp := string(byt)
+
+	pattern := os.Args[1]
 	count := 0
 
 	//bad match table will be a map which will contain skip values for each of the runes from patter
@@ -15,52 +28,52 @@ func main() {
 
 	l := len(pattern)
 
-	for i,c := range pattern {
+	for i, c := range pattern {
 		bm[c] = l - i - 1
-		if i == l - 1 {
+		if i == l-1 {
 			bm[c] = l
 		}
 	}
 
 	//cursor starts from the end of the pattern as the matching is always done from right to left
 	cursor := l - 1
-	//trimmer := 0
+	index := 0
 
-	for j := 0; j < len(str); j++ {
-
-		if str[cursor] != pattern[cursor] {
-
-			if bm[rune(str[cursor])] == 0 {
-				str = str[l:]
-				//trimmer = trimmer + l
-				//j = j + l
-				continue
+LOOP:
+	if str[cursor] == pattern[cursor] {
+		x := bm[rune(str[cursor])]
+		for i := cursor; i >= 0; i-- {
+			if str[i] == pattern[i] {
+				count++
+			} else {
+				count = 0
+				str = str[x:]
+				index = index + x
+				goto LOOP
 			}
-			//trim str
-			//j = j  + bm[rune(str[cursor])]
-			indexToTrim  := bm[rune(str[cursor])]
-			str = str[indexToTrim:]
-			//trimmer = trimmer+indexToTrim
-			continue
 		}
+		if count == l {
+			word := color.RedString(str[:l])
+			result := fmt.Sprintf("%s %s %s",strcp[:index-l],word,strcp[index +1:])
+			fmt.Println(result)
+			return
+		}
+		fmt.Println("not found")
+	}
 
-		if str[cursor] == pattern[cursor] {
+	if str[cursor] != pattern[cursor] {
 
-			for i := cursor; i >= 0; i-- {
-				if str[i] == pattern[i]{
-					count++
-					if count == l {
-						fmt.Printf("found at index: %d, result: %s pattern: %s \n", 0, str, pattern)
-						return
-					}
-				}
+		if bm[rune(str[cursor])] == 0 {
+			str = str[l:]
+			index = index + l
 
-
-
-			}
+		} else {
+			trim := bm[rune(str[cursor])]
+			str = str[trim:]
+			index = index + bm[rune(str[cursor])]
 
 		}
-
+		goto LOOP
 	}
 
 }
